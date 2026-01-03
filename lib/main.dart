1479dd29078d322781f86dc38dca7d8d1d725679
Ahmed Omran <mamdouhanahamada@gmail.com>
2026-01-03 14:04:36 +0200

    added firebase to the project to test firebase app distribution

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:cache_and_theme_task_mentorship/core/services/prefs.dart';
 import 'package:cache_and_theme_task_mentorship/core/theme_cubit/theme_cubit.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
@@ -7,13 +8,14 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'cache_and_theme_task_mentorship.dart';
 import 'core/services/setup_service_locator.dart';
 import 'features/products/data/models/product_model.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await Hive.initFlutter();
   await Prefs.init();
   Hive.registerAdapter(ProductModelAdapter());
-
   setupServiceLocator();
   runApp(
     BlocProvider(
