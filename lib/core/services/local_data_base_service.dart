29f5b155cb0941dee3781260f5b98c6ae2e1fcf4
AhmedOmran22 <mamdouhanahamada@gmail.com>
2026-01-27 12:08:35 +0200

    fix: resolve HiveError by managing box lifecycle correctly in LocalDataBaseService

--- lib/core/services/local_data_base_service.dart
+++ lib/core/services/local_data_base_service.dart
@@ -1,33 +1,37 @@
 import 'package:hive/hive.dart';
 
 class LocalDataBaseService {
+  /// Open box helper to ensure box is open and accessible
+  Future<Box<T>> _getBox<T>(String boxName) async {
+    if (Hive.isBoxOpen(boxName)) {
+      return Hive.box<T>(boxName);
+    }
+    return await Hive.openBox<T>(boxName);
+  }
+
   /// Save data in box by name
   Future<void> saveData<T>(String boxName, List<T> data) async {
-    final box = await Hive.openBox<T>(boxName);
+    final box = await _getBox<T>(boxName);
     await box.clear(); // Clear old data
     await box.addAll(data);
-    await box.close();
   }
 
   /// Add data to existing box
   Future<void> addData<T>(String boxName, List<T> data) async {
-    final box = await Hive.openBox<T>(boxName);
+    final box = await _getBox<T>(boxName);
     await box.addAll(data);
-    await box.close();
   }
 
   /// Get data from box by name
   Future<List<T>> getData<T>(String boxName) async {
-    final box = await Hive.openBox<T>(boxName);
+    final box = await _getBox<T>(boxName);
     final data = box.values.toList();
-    await box.close();
     return data;
   }
 
   /// Clear All data from the Box
   Future<void> clearBox(String boxName) async {
-    final box = await Hive.openBox(boxName);
+    final box = await _getBox(boxName);
     await box.clear();
-    await box.close();
   }
 }
